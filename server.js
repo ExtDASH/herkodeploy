d1a824c11cde380c5403ef5f8c3541e557f8d4a0
ExtDASH <rpoynter@comcast.net>
2018-09-14 12:26:29 -0600

    server change

--- server.js
+++ server.js
@@ -84,5 +84,8 @@ app.use('/fileGet', namesRouter)
 
 mongoose.connect('mongodb://allclients:allclients1@ds021172.mlab.com:21172/yodeldidschecker', { useNewUrlParser: true })
 	.then(() => {
-		app.listen(3000)
+		const PORT = process.env.PORT || 3000;
+		app.listen(PORT, () => {
+		    console.log(`Our app is running on port ${ PORT }`);
+		});
 	})
